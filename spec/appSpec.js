051469fc12406576e82438d809bbe32630581be6
Libby <librod89@gmail.com>
2016-10-25 14:47:35 -0400

    change file structure of lib files. Add Jasmine testing framework.

---
+++ spec/appSpec.js
@@ -0,0 +1,12 @@
+describe('Controller', function(){
+    var controller;
+    
+    beforeEach(function(){
+       controller = new Controller(); 
+    });
+    
+    it('should be defined', function(){
+        expect(controller).toBeDefined();
+    });
+    
+});
